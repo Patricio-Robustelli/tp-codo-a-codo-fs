d0f6f5fdb0f251e48da3f31abe3fe4ecf2f25c3a
Patricio <patriciorobustelli88@gmail.com>
2023-10-09 00:21:51 -0300

    falta terminar el footer

---
+++ js/java.js
@@ -0,0 +1,12 @@
+let footer = ` <div class="pie"><br>
+<div align="center"><img class="iconos" src="img/twitter.png">
+<img class="iconos" src="img/facebook.png">
+<img class="iconos" src="img/pinterest.png">
+<img class="iconos" src="img/instagram.png">
+<img class="iconos" src="img/linkedin.png"></div>
+
+
+<h5 align="center">Derechos reservados @2023</h5>
+</div>   
+`
+document.getElementById("footer").innerHTML = footer    
