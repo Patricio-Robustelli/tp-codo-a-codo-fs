0281ab8eaab16db55e75b206bd73499ab051bc1f
Luis Teran <luigykun@gmail.com>
2023-10-16 12:52:34 -0300

    titulo mas grueso, quite el marge superior

--- js/java.js
+++ js/java.js
@@ -19,15 +19,15 @@ document.getElementById("footer").innerHTML = footer*/
 let footer = ` 
 
 
-<div class="pie">
-            <div>
-              <a href="https://www.facebook.com/?locale=es_LA"  target="_blank"><img class="iconfoot" src="img/facebook.png"></a>
+
+  <div>
+    <a href="https://www.facebook.com/?locale=es_LA"  target="_blank"><img class="iconfoot" src="img/facebook.png"></a>
             
-              <a href="https://www.instagram.com/"  target="_blank"><img class="iconfoot" src="img/instagram.png"></a>
-            </div>
+    <a href="https://www.instagram.com/"  target="_blank"><img class="iconfoot" src="img/instagram.png"></a>
+    </div>
             
-            <h5>Derechos reservados @2023</h5>
-        </div>   
+  <h5>Derechos reservados @2023</h5>
+          
 
 `
 document.getElementById("footer").innerHTML = footer
