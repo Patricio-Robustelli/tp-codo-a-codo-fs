b6f8c2a708582d78fb2ce1eb2e2cd090b05dd329
Luis Teran <145719354+luigykun@users.noreply.github.com>
2023-10-20 23:01:58 -0300

    Revert "Merge branch 'main' of https://github.com/Patricio-Robustelli/tp-codo-a-codo-fs"
    This reverts commit 0ae0cf544e9783d5ff98a0cabcd7959ad42e0241, reversing
    changes made to ef810e14fbef298166e61af4433c9a9c03aa8e5f.

--- js/java.js
+++ js/java.js
@@ -7,8 +7,8 @@ let header = `
 </div>
 
 <div class="logo">
-    <img id=trebol src="img/trebol.ico" alt=""> <a href="./index.html"></a>
-    <div id="nombre"><a href="index.html">La Fortuna</a></div>
+    <img src="img/trebol.ico" alt=""> <a href="./index.html"></a>
+    <div class="nombre"><a href="index.html">La Fortuna </a></div>
 </div>
 
 <nav>
